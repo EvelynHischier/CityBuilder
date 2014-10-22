c7321f06416d57b501da923976d0dfd5c2dcde59
mehdi-valette <mehdi.valette@gmail.com>
2014-10-22 15:06:43 +0200

    Simplification and prepared statement

--- ServerSide/model/Class.Dictionary.php
+++ ServerSide/model/Class.Dictionary.php
@@ -1,64 +1,22 @@
 <?php
-function __autoload($class){
-	include_once __DIR__."/Class.".$class.".php";
-}
 class Dictionary{
-	private $_itemType;
-	private $_key;
-	private $_language;
-	private $_text;
 	private $_pdo = null;
 	
 	public function __construct() {
 		$this->_pdo = $GLOBALS["pdo"];
 	}
 	
-	public static function newTranslation($key,$language,$text){
-		$instance = new self();
-		$instance->setKey($key);
-		$instance->setLanguage($language);
-		$instance->setText($text);
-		//$instance->_itemType=new ItemType();
-		return $instance;
-	}
-	public function getKey(){
-		return $this->_key;
-	}
-	public function setKey($key){
-		$this->_key=$key;
-	}
-	public function getLanguage(){
-		return $this->_language;
-	}
-	public function setLanguage($language){
-		$this->_language=$language;
-	}
-	public function getText(){
-		return $this->_text;
-	}
-	public function setText($text){
-		$this->_text=$text;
-	}
-	
 	public function getError(){
 		if($this->_pdo->errorCode()!='00000')
 			return 'Query failed';
 	}
-	public function getDictionary($key,$language){
-		//$texts = array();
-		$query="select Language, `key`, `text` from Dictionary where `Language` = \"fr\"";
-		//$pdo = $GLOBALS["pdo"];
+	
+	public function getDictionary($language){
+		$query="select language, `key`, `text` from Dictionary where `Language` = ?";
 		
-		$result= $this->_pdo->query($query);
+		$result= $this->_pdo->prepare($query);
+		$result->execute(array($language));
 		$texts = $result->fetchAll(PDO::FETCH_ASSOC);
-// 		/*if($this->getError())
-// 			trigger_error($this->getError());*/
-// 		while($row =$result->fetch()){
-// 			$texts[0]=$row['Key'];
-// 			$texts[1]=$row['Language'];
-// 			$texts[2]=$row['Text'];
-// 		}
-// 		$result->closeCursor();
 
 		return $texts;
 	}

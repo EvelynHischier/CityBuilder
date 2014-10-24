84c914c92b97c115f04d5bc852dba58e5fb7760e
maniac1989 <pierry_wyssen@hotmail.com>
2014-10-24 15:25:11 +0200

    removed comments

--- ServerSide/model/Class.ItemType.php
+++ ServerSide/model/Class.ItemType.php
@@ -1,16 +1,8 @@
 <?php
 class ItemType{
-	/*private $_name;
-	private $_description;
-	private $_min;
-	private $_max;*/
 	private $_pdo;
 	
-	public function __construct(){
-		/*$this->setName($name);
-		$this->setDescription($description);
-		$this->setMin($min);
-		$this->setMax($max);*/
+	public function __construct() {
 		$this->_pdo = $GLOBALS["pdo"];
 	}
 	
@@ -18,31 +10,7 @@ class ItemType{
 		if($this->_pdo->errorCode()!='00000')
 			return 'Query failed';
 	}
-	
-	/*public function getDescription(){
-		return $this->_description;
-	}
-	public function setDescription($description){
-		$this->_description=$description;
-	}
-	public function getName(){
-		return $this->_name;
-	}
-	public function setName($name){
-		$this->_name=$name;
-	}
-	public function getMin(){
-		return $this->_min;
-	}
-	public function setMin($min){
-		$this->_min=$min;
-	}
-	public function getMax(){
-		return $this->_max;
-	}
-	public function setMax($max){
-		$this->_max=$max;
-	}*/
+
 	public function getItemTypes() {
 		$query = "SELECT `ItemTypeID`, `Name`, `Min`, `Max`, `Description` FROM ItemType";
 		

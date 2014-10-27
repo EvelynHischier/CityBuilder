924f48e0c99ac463dc6237cc4a222f06fb424ebe
maniac1989 <pierry_wyssen@hotmail.com>
2014-10-27 14:00:40 +0100

    removed comments

--- ServerSide/model/Class.ChosenMode.php
+++ ServerSide/model/Class.ChosenMode.php
@@ -3,13 +3,10 @@
 	include_once __DIR__."../model/Class.".$class."php";
 }*/
 class ChosenMode{
-	/*private $_time;
-	private $_mode;*/
+
 	private $_pdo;
 	
 	public function __construct() {
-		/*$this->setTime($time);
-		$this->_mode=new Mode();*/
 		$this->_pdo = $GLOBALS["pdo"];
 	}
 	
@@ -17,13 +14,6 @@ class ChosenMode{
 		if($this->_pdo->errorCode()!='00000')
 			return 'Query failed';
 	}
-	
-	/*public function getTime(){
-		return $this->_time;
-	}
-	public function setTime($time){
-		$this->_time=$time;
-	}*/
 	
 	public function getChosenModes() {
 		$query = "SELECT `Mode_ModeID`, `Time` FROM ChosenMode";

74660799916dfe09b80a743919e8e882dcb1bb6d
maniac1989 <pierry_wyssen@hotmail.com>
2014-10-27 13:24:50 +0100

    Deleted comments

--- ServerSide/model/Class.Game.php
+++ ServerSide/model/Class.Game.php
@@ -3,19 +3,10 @@
 	include_once __DIR__."/Class.".$class."php";
 }*/
 class Game {
-	/*private $_mode;
-	private $_user;
-	private $_zone;
-	private $_start;
-	private $_end;*/
+
 	private $_pdo;
 	
 	public function __construct() {
-		/*$now = new DateTime();
-		$this->setStart($now->format('Y-m-d H:i:s'));
-		$this->_mode=new Mode();
-		$this->_user=new User();
-		$this->_zone=new Zone();*/
 		$this->_pdo = $GLOBALS["pdo"];
 	}
 	
@@ -23,22 +14,6 @@ class Game {
 		if($this->_pdo->errorCode()!='00000')
 			return 'Query failed';
 	}
-	
-	/*public function getStart(){
-		return $this->_start;
-	}
-	
-	public function setStart($start){
-		$this->_start=$start;
-	}
-	
-	public function getEnd(){
-		return $this->_end;
-	}
-	
-	public function setEnd($end){
-		$this->_end=$end;
-	}*/
 	
 	public function getGames() {
 		$query = "SELECT `Mode_ModeID`, `Zone_ZoneID`, `User_UserID`, `Start`, `End` FROM Game";

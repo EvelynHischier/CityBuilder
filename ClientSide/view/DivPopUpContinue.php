6582aa16b5f9d08a9e30dd41c2d15f9397067f11
EvelynHischier <evelyn_hischier@hotmail.com>
2014-10-27 15:42:53 +0100

    Popup styling part 2

--- ClientSide/view/DivPopUpContinue.php
+++ ClientSide/view/DivPopUpContinue.php
@@ -1,7 +1,13 @@
-<div id="popupContinue" class="divPopup"   data-ng-show="popup=='continue'">
+<div id="popupContinue" class="divPopup"
+	data-ng-show="popup=='continue'">
 
-	<div class="popupImage" id="popupImageContinue"></div>
-	<div class="popupText" id="popupTextContinue"></div>
-	<div class="popupButton" id="popupButtonContinue"></div>
+	<div class="popupImage" id="popupImageContinue">
+		<img alt="picture" src="pictures/management5.png">
+	</div>
+	<div class="popupText" id="popupTextContinue">bla bla bla bla bla blab
+		bla bla bla</div>
+	<div class="popupButton" id="popupButtonContinue">
+		<input type="button" id="popupNext" value="Continue">
+	</div>
 
 </div>
--- ClientSide/view/DivPopUpYes.php
+++ ClientSide/view/DivPopUpYes.php
@@ -3,7 +3,10 @@
 	<div class="popupImage" id="popupImageYes">
 		<img alt="picture" src="pictures/management5.png">
 	</div>
-	<div class="popupText" id="popupTextYes"></div>
-	<div class="popupButton" id="popupButtonYes"></div>
+	<div class="popupText" id="popupTextYes">fsjkdfhdsj fhsdfk sdhkfj sfh skdjfh ksjdfh ksjsjdhfkj sdh fsdhf skdfjh skdjfh sdjf.</div>
+	<div class="popupButton" id="popupButtonYesNo">
+	<input type="button" id="popupYes" class="twoButtons" value="Yes"/>
+	<input type="button" id="popupNo" class="twoButtons" value="No"/>
+	</div>
 
 </div>

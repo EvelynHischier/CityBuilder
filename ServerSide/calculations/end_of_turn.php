bbfdec8aa0f94408bc1557c7e393ae52fd19ecbc
maniac1989 <pierry_wyssen@hotmail.com>
2014-10-31 13:27:25 +0100

    completely reorganized the end_of_turn.php file (sequence => class)

--- ServerSide/calculations/end_of_turn.php
+++ ServerSide/calculations/end_of_turn.php
@@ -1,13 +1,13 @@
 <?php
 	require_once( "../model/Class.Historic.php");
 	
-	session_start();
+	/*session_start();
 	
 	$pdo = new PDO("mysql:host=db4free.net;
 				port=3306;
 				dbname=pyramidgame1",
 				"groupe1",
-				"8?Wzgr10");
+				"8?Wzgr10");*/
 	/*
 	 * Social pyramid
 	 * 				Array Index
@@ -29,116 +29,141 @@
 	 * 
 	 */
 
-
+class Calculation {
 	/*************************VARIABLES*************************/
 	
-	$popTotal = 2000;
-	$wealthTotal = 500; //population divided by 4
-	$nbrClasses = array(4,50,100,620,500,500,16); //1790
-	$nbrClassesSum = array_sum($nbrClasses);
-	$invasion = false;
-	$potteryResearched = 0;
-	$granaryResearched = 0;
-	$writingResearched = 0;
-	$unhappiness = false;
-	$nbrCaravans = 2;
-	$food = 1000; //population divided by 2
-	$foodProduction = 0;
-	$templeBuilt = 0;
-	$palaceBuilt = 0;
-	$monumentBuilt = 0;
+	private $_popTotal;
+	private $_wealthTotal;
+	private $_nbrClasses;
+	private $_nbrClassesSum;
+	private $_invasion;
+	private $_potteryResearched;
+	private $_granaryResearched;
+	private $_writingResearched;
+	private $_unhappiness;
+	private $_nbrCaravans;
+	private $_food;
+	private $_foodProduction;
+	private $_templeBuilt;
+	private $_palaceBuilt;
+	private $_monumentBuilt;
+	private $_hist;
+	
+	public function __construct($popTotal, $wealthTotal, $nbrClasses, $food, $nbrCaravans) {
+		$this->_popTotal = 2000;
+		$this->_wealthTotal = 500; //population divided by 4
+		$this->_nbrClasses = array(4,50,100,620,500,500,16); //1790
+		$this->_nbrClassesSum = array_sum($this->_nbrClasses);
+		$this->_invasion = false;
+		$this->_potteryResearched = 0;
+		$this->_granaryResearched = 0;
+		$this->_writingResearched = 0;
+		$this->_unhappiness = false;
+		$this->_nbrCaravans = 2;
+		$this->_food = 1000; //population divided by 2
+		$this->_foodProduction = 0;
+		$this->_templeBuilt = 0;
+		$this->_palaceBuilt = 0;
+		$this->_monumentBuilt = 0;
+		
+		$this->_hist = new Historic();
+		
+		insertIntoDB();
+		invasion();
+		
+		//1) Unhappiness
+		$unhappiness = calculateUnhappiness($this->_nbrClasses);
+		//2) wealth
+		$wealthTotal += calculateWealth($this->_nbrClasses, $this->_potteryResearched);
+		//3) caravan
+		if(calculateCaravan($this->_wealthTotal))
+			$this->_nbrCaravans++;
+		//4) building
+		checkBuildings($this->_wealthTotal, $this->_nbrClasses);
+		//5) food
+		$foodProduction = calculateFoodProduction($this->_nbrClasses,$this->_unhappiness);
+		$foodConsumption = $this->_nbrClassesSum;
+		$food = calculateRemainingFood($this->_granaryResearched, $this->_food, $this->_foodConsumption, $this->_foodProduction);
+		//6) population
+		$newTotalPopulation = calculateNewTotalPopulation($this->_food, $this->_popTotal, $this->_foodProduction);
+	}
 	
 	/*************************DB ACCESS*************************/
 	
 	//INSERT INTO DB - Table Historic
-	$hist = new Historic();
+	
 	
 	//Parameter order
 	//$game,$turn,$kings,$priests,$scribes,$soldiers,$slaves,$peasants,$craftsmen,$population,$wealth,$food,
 	//$time,$score,$pottery,$granary,$writing,$caravans,$temple,$palace,$monument
 	
-	$hist->insertHistoric(1,1,$nbrClasses[0],$nbrClasses[1],$nbrClasses[2],$nbrClasses[3],$nbrClasses[5],$nbrClasses[4],
-			$nbrClasses[6],$nbrClassesSum,$wealthTotal,$food,'00:04:04',0,$potteryResearched,$granaryResearched,
-			$writingResearched,$nbrCaravans,$templeBuilt,$palaceBuilt,$monumentBuilt);
-	
-	
-	/*************************CALCULATIONS*************************/
-	echo "<p>Wealth: " . $wealthTotal . "</p>";
-	echo "<p>Food: " . $food . "</p>";
-	
-	//Calculate invasion
-	if($nbrClasses[3]<$popTotal*0.025) //number of soldiers < 2.5%
-		$invasion = true;
-	
-	//Calculate losses
-	if($invasion) {
-		$lostPop = (int) ((3-($nbrClasses[3]*100)/$popTotal)*5)*$popTotal/100;
-		$lostWealth = (int) ((3-($nbrClasses[3]*100)/$popTotal)*15)*$wealthTotal/100;
-		
-		echo "<p>Population lost: " . $lostPop . "</p>";
-		echo "<p>Wealth lost: " . $lostWealth . "</p>";
-		
-		//Apply the losses
-		$wealthTotal -= $lostWealth;
-
-		$nbrClasses[3] -= $lostPop; //example: -13800 => 0, number of soldiers
-		if($nbrClasses[3] < 0) {
-			$nbrClasses[4] += $nbrClasses[3]; //-3800, number of peasants
-			$nbrClasses[3] = 0;
-		}
-		
-		if($nbrClasses[4] < 0) {
-			$nbrClasses[5] += $nbrClasses[4]; //1200, number of slaves
-			$nbrClasses[4] = 0;
-		}
-		
-		if($nbrClasses[5] < 0) { //false, 1200 is greater than 0
-			$nbrClasses[2] += $nbrClasses[5]; //number of scribes
-			$nbrClasses[5] = 0;
-		}
-		
-		if($nbrClasses[2] < 0) {
-			$nbrClasses[1] += $nbrClasses[2]; //number of priests
-			$nbrClasses[2] = 0;
-		}
-		
-		if($nbrClasses[1] < 0) {
-			$nbrClasses[0] += $nbrClasses[1]; //number of kings
-			$nbrClasses[1] = 0;
-		}
-		
-		if($nbrClasses[0] < 0) {
-			$nbrClasses[0] = 0;
-		}
-	} else {
-		$lostPop = 0;
-		$lostWealth = 0;
+	public function insertIntoDB() {
+		$this->_hist->insertHistoric(1,1,$this->_nbrClasses[0],$this->_nbrClasses[1],$this->_nbrClasses[2],
+			$this->_nbrClasses[3],$this->_nbrClasses[5],$this->_nbrClasses[4],$this->_nbrClasses[6],
+			$this->_nbrClassesSum,$this->_wealthTotal,$this->_food,'00:04:04',0,$this->_potteryResearched,
+			$this->_granaryResearched,$this->_writingResearched,$this->_nbrCaravans,$this->_templeBuilt,
+			$this->_palaceBuilt,$this->_monumentBuilt);
 	}
 	
-	//1) Unhappiness
-	$unhappiness = calculateUnhappiness($nbrClasses);
-	//2) wealth
-	$wealthTotal += calculateWealth($nbrClasses, $potteryResearched);
-	//3) caravan
-	if(calculateCaravan($wealthTotal))
-		$nbrCaravans++;
-	//4) building
-	checkBuildings($wealthTotal, $nbrClasses);
-	//5) food
-	$foodProduction = calculateFoodProduction($nbrClasses,$unhappiness);
-	$foodConsumption = $nbrClassesSum;
-	$food = calculateRemainingFood($granaryResearched, $food, $foodConsumption, $foodProduction);
-	//6) population
-	$newTotalPopulation = calculateNewTotalPopulation($food, $popTotal, $foodProduction);
 	
+	public function invasion() {
+		if($this->_nbrClasses[3]<$this->_popTotal*0.025) //number of soldiers < 2.5%
+			$this->_invasion = true;
+		
+		//Calculate losses
+		if($this->_invasion) {
+			$this->_lostPop = (int) ((3-($this->_nbrClasses[3]*100)/$this->_popTotal)*5)*$this->_popTotal/100;
+			$this->_lostWealth = (int) ((3-($this->_nbrClasses[3]*100)/$this->_popTotal)*15)*$this->_wealthTotal/100;
+		
+			//echo "<p>Population lost: " . $lostPop . "</p>";
+			//echo "<p>Wealth lost: " . $lostWealth . "</p>";
+		
+			//Apply the losses
+			$this->_wealthTotal -= $this->_lostWealth;
+		
+			$this->_nbrClasses[3] -= $this->_lostPop; //example: -13800 => 0, number of soldiers
+			if($this->_nbrClasses[3] < 0) {
+				$this->_nbrClasses[4] += $this->_nbrClasses[3]; //-3800, number of peasants
+				$this->_nbrClasses[3] = 0;
+			}
+		
+			if($this->_nbrClasses[4] < 0) {
+				$this->_nbrClasses[5] += $this->_nbrClasses[4]; //1200, number of slaves
+				$this->_nbrClasses[4] = 0;
+			}
+		
+			if($this->_nbrClasses[5] < 0) { //false, 1200 is greater than 0
+				$this->_nbrClasses[2] += $this->_nbrClasses[5]; //number of scribes
+				$this->_nbrClasses[5] = 0;
+			}
+		
+			if($this->_nbrClasses[2] < 0) {
+				$this->_nbrClasses[1] += $this->_nbrClasses[2]; //number of priests
+				$this->_nbrClasses[2] = 0;
+			}
+		
+			if($this->_nbrClasses[1] < 0) {
+				$this->_nbrClasses[0] += $this->_nbrClasses[1]; //number of kings
+				$this->_nbrClasses[1] = 0;
+			}
+		
+			if($this->_nbrClasses[0] < 0) {
+				$this->_nbrClasses[0] = 0;
+			}
+		} else {
+			$this->_lostPop = 0;
+			$this->_lostWealth = 0;
+		}
+	}
+
 	/*************************UNHAPPINESS*************************/
 	
 	function calculateUnhappiness($nbrClasses) {
 		$unhappiness = false;
 		
-		if($nbrClasses[0] > 1 || $nbrClasses[0] < 1 || 
-			$nbrClasses[1] <= array_sum($nbrClasses)*0.0025 ||
-			$nbrClasses[5] <= array_sum($nbrClasses)*0.02)
+		if($this->_nbrClasses[0] > 1 || $this->_nbrClasses[0] < 1 || 
+			$this->_nbrClasses[1] <= array_sum($this->_nbrClasses)*0.0025 ||
+			$this->_nbrClasses[5] <= array_sum($this->_nbrClasses)*0.02)
 		{
 			echo "Unhappiness = 1!";
 			$unhappiness = true;
@@ -155,7 +180,7 @@
 		if($potteryResearched)
 			$potteryValue = 2;
 		
-		$producedWealth = $nbrClasses[6] * (10 + $potteryValue);
+		$producedWealth = $this->_nbrClasses[6] * (10 + $potteryValue);
 		return $producedWealth;
 	}
 	
@@ -169,13 +194,13 @@
 	/*************************BUILDINGS*************************/
 	
 	function checkBuildings($wealthTotal, $nbrClasses) {
-		if($nbrClasses[1] >= 10 && $wealthTotal >= 550 && $nbrClasses[4] >= 1000)
+		if($this->_nbrClasses[1] >= 10 && $this->_wealthTotal >= 550 && $this->_nbrClasses[4] >= 1000)
 			echo "<p>Building temple...</p>"; //set templeBuilt to 1
 		
-		if($wealthTotal >= 850 && $nbrClasses[4] >= 1500)
+		if($this->_wealthTotal >= 850 && $this->_nbrClasses[4] >= 1500)
 			echo "<p>Building Palace...</p>"; //set palaceBuilt to 1
 		
-		if($wealthTotal >= 1150 && $nbrClasses[4] >= 1900)
+		if($this->_wealthTotal >= 1150 && $this->_nbrClasses[4] >= 1900)
 			echo "<p>Building monument...</p>"; //set monumentBuilt to 1
 		
 	}
@@ -186,7 +211,7 @@
 		//100/2000 = 5%
 		$unhappinessFactor = 1;
 		
-		$scribesFactor = ($nbrClasses[2]/array_sum($nbrClasses))*100*(1/36); //
+		$scribesFactor = ($this->_nbrClasses[2]/array_sum($this->_nbrClasses))*100*(1/36); //
 		if($scribesFactor > (1/36))
 			$scribesFactor = 1/36;
 		
@@ -198,7 +223,7 @@
 		
 		//Example: (peasants: 500, unhappiness = true, number of scribes: 100)
 		//500*0.75*((10/9)+(1/36)) = 500*0.75*1.138888
-		$foodProduction = (int) ($nbrClasses[4]*$unhappinessFactor*((10/9)+$scribesFactor));
+		$foodProduction = (int) ($this->_nbrClasses[4]*$unhappinessFactor*((10/9)+$scribesFactor));
 		
 		return $foodProduction;
 	}
@@ -226,9 +251,10 @@
 		
 		return $newTotalPopulation;
 	}
+}
 	
 ?>
-
+<!--
 <html>
 <head>
 <title>Testing</title>
@@ -238,75 +264,75 @@
 <table>
 	<tr>
 		<td>Invasion:</td>
-		<td><?php var_dump($invasion); ?></td>
+		<td><?php //var_dump($invasion); ?></td>
 	</tr>
 	<tr>
 		<td>Total population</td>
-		<td><?php var_dump($popTotal); ?></td>
+		<td><?php //var_dump($popTotal); ?></td>
 	</tr>
 	<tr>
 		<td>Population assigned:</td>
-		<td><?php var_dump($nbrClassesSum); ?></td>
+		<td><?php //var_dump($this->_nbrClassesSum); ?></td>
 	</tr>
 	<tr>
 		<td>Remaining wealth:</td>
-		<td><?php var_dump($wealthTotal); ?></td>
+		<td><?php //var_dump($wealthTotal); ?></td>
 	</tr>
 	<tr>
 		<td>Soldiers:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[3]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[3]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Peasants:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[4]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[4]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Slaves:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[5]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[5]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Craftsmen:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[6]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[6]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Scribes:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[2]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[2]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Priests:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[1]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[1]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Kings:</td>
-		<td><input type="number" value="<?php echo $nbrClasses[0]; ?>" /></td>
+		<td><input type="number" value="<?php //echo $this->_nbrClasses[0]; ?>" /></td>
 	</tr>
 	<tr>
 		<td>Population lost:</td>
-		<td><?php echo $lostPop; ?></td>
+		<td><?php //echo $lostPop; ?></td>
 	</tr>
 	<tr>
 		<td>Remaining population:</td>
-		<td><?php echo array_sum($nbrClasses); ?></td>
+		<td><?php //echo array_sum($this->_nbrClasses); ?></td>
 	</tr>
 	<tr>
 		<td>Wealth lost:</td>
-		<td><?php echo $lostWealth; ?></td>
+		<td><?php //echo $lostWealth; ?></td>
 	</tr>
 	<tr>
 		<td>Food production:</td>
-		<td><?php echo $foodProduction; ?></td>
+		<td><?php //echo $foodProduction; ?></td>
 	</tr>
 	<tr>
 		<td>Food consumption:</td>
-		<td><?php echo $foodConsumption; ?></td>
+		<td><?php //echo $foodConsumption; ?></td>
 	</tr>
 	<tr>
 		<td>Remaining food:</td>
-		<td><?php echo $food; ?></td>
+		<td><?php //echo $food; ?></td>
 	</tr>
 	<tr>
 		<td>New total population:</td>
-		<td><?php echo $newTotalPopulation; ?></td>
+		<td><?php //echo $newTotalPopulation; ?></td>
 	</tr>
 </table>
 <input type="button" value="Next round" onclick="nextRound()" />
@@ -320,3 +346,4 @@
 
 </body>
 </html>
+ -->

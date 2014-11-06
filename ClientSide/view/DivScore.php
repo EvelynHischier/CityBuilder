106849e8e7066b9bdaa10dcced310b2717679019
maniac1989 <pierry_wyssen@hotmail.com>
2014-11-06 16:21:55 +0100

    added mouse-over effects for the <td>'s <input> fields

--- ClientSide/view/DivScore.php
+++ ClientSide/view/DivScore.php
@@ -13,28 +13,43 @@
 			<tr>
 <!-- 			Wealth -->
 				<td class="scoreName" id="table_score"  data-ng-bind="scoreWealthTxt" data-ng-mouseover="hoverScore('wealth')"></td>
-				<td class="scoreNumber"><input type="text" id="table_wealth_number" readonly="readonly" data-ng-value="scoreWealth"></td>
+				<td class="scoreNumber">
+					<input type="text" id="table_wealth_number" readonly="readonly" 
+					data-ng-value="scoreWealth" data-ng-mouseover="hoverScore('wealth')">
+				</td>
 			</tr>
 			<tr>
 <!-- 			Buildings -->
 				<td class="scoreName" id="table_score" data-ng-bind="scoreBuildingsTxt" data-ng-mouseover="hoverScore('buildings')"></td>
-				<td class="scoreNumber"><input type="text" id="table_buildings_number" readonly="readonly" data-ng-value="scoreBuildings"></td>
+				<td class="scoreNumber">
+					<input type="text" id="table_buildings_number" readonly="readonly" 
+					data-ng-value="scoreBuildings" data-ng-mouseover="hoverScore('buildings')">
+				</td>
 			</tr>
 			<tr>
 <!-- 			Population -->
 				<td class="scoreName" id="table_score" data-ng-bind="scorePopulationTxt" data-ng-mouseover="hoverScore('population')"></td>
-				<td class="scoreNumber"><input type="text" id="table_population_number" readonly="readonly" data-ng-value="scorePopulation"></td>
+				<td class="scoreNumber">
+					<input type="text" id="table_population_number" readonly="readonly" 
+					data-ng-value="scorePopulation" hoverScore('population')">
+				</td>
 			</tr>
 			<tr>
 <!-- 			Happiness -->
 				<td class="scoreName" id="table_score" data-ng-bind="scoreHappinessTxt" data-ng-mouseover="hoverScore('happiness')"></td>
-				<td class="scoreNumber"><input type="text" id="table_happyness_number" readonly="readonly" data-ng-value="scoreHappyness"></td>
+				<td class="scoreNumber">
+					<input type="text" id="table_happyness_number" readonly="readonly" 
+					data-ng-value="scoreHappyness" data-ng-mouseover="hoverScore('happiness')">
+				</td>
 			</tr>
 			<tr class="scoreName"></tr>
 			<tr>
 <!-- 			Total -->
 				<td class="scoreName" id="table_score" data-ng-bind="scoreTotalTxt" data-ng-mouseover="hoverScore('total')"></td>
-				<td class="scoreNumber"><input type="text" id="table_total_number" readonly="readonly" data-ng-value="scoreTotal"></td>
+				<td class="scoreNumber">
+					<input type="text" id="table_total_number" readonly="readonly" 
+					data-ng-value="scoreTotal" data-ng-mouseover="hoverScore('total')">
+				</td>
 			</tr>
 		</tbody>
 

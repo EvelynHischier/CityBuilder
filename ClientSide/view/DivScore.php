590c3051519164ee9fcc8b15af177b6622b777de
maniac1989 <pierry_wyssen@hotmail.com>
2014-11-06 11:19:15 +0100

    added table_score in css and made the score table look fabulous!!

--- ClientSide/view/DivScore.php
+++ ClientSide/view/DivScore.php
@@ -4,7 +4,7 @@
 		<tbody>
 			<tr>
 <!-- 			Technologie -->
-				<td class="scoreName" data-ng-bind="scoreTechnologyTxt"></td>
+				<td class="scoreName" id="table_score" data-ng-bind="scoreTechnologyTxt"></td>
 				<td class="scoreNumber"><input type="text" id="table_technology_number" readonly="readonly" data-ng-value="scoreTechnology"></td>
 				
 <!-- 				some text to display -->
@@ -12,28 +12,28 @@
 			</tr>
 			<tr>
 <!-- 			Wealth -->
-				<td class="scoreName"  data-ng-bind="scoreWealthTxt"></td>
+				<td class="scoreName" id="table_score"  data-ng-bind="scoreWealthTxt"></td>
 				<td class="scoreNumber"><input type="text" id="table_wealth_number" readonly="readonly" data-ng-value="scoreWealth"></td>
 			</tr>
 			<tr>
 <!-- 			Buildings -->
-				<td class="scoreName" data-ng-bind="scoreBuildingsTxt"></td>
+				<td class="scoreName" id="table_score" data-ng-bind="scoreBuildingsTxt"></td>
 				<td class="scoreNumber"><input type="text" id="table_buildings_number" readonly="readonly" data-ng-value="scoreBuildings"></td>
 			</tr>
 			<tr>
 <!-- 			Population -->
-				<td class="scoreName" data-ng-bind="scorePopulationTxt"></td>
+				<td class="scoreName" id="table_score" data-ng-bind="scorePopulationTxt"></td>
 				<td class="scoreNumber"><input type="text" id="table_population_number" readonly="readonly" data-ng-value="scorePopulation"></td>
 			</tr>
 			<tr>
 <!-- 			Happiness -->
-				<td class="scoreName" data-ng-bind="scoreHappinessTxt"></td>
+				<td class="scoreName" id="table_score" data-ng-bind="scoreHappinessTxt"></td>
 				<td class="scoreNumber"><input type="text" id="table_happyness_number" readonly="readonly" data-ng-value="scoreHappyness"></td>
 			</tr>
 			<tr class="scoreName"></tr>
 			<tr>
 <!-- 			Total -->
-				<td class="scoreName" data-ng-bind="scoreTotalTxt"></td>
+				<td class="scoreName" id="table_score" data-ng-bind="scoreTotalTxt"></td>
 				<td class="scoreNumber"><input type="text" id="table_total_number" readonly="readonly" data-ng-value="scoreTotal"></td>
 			</tr>
 		</tbody>

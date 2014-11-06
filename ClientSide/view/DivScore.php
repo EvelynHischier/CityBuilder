adcfc853ccf0b6a4ad8428204daf27ffdd08ea6e
maniac1989 <pierry_wyssen@hotmail.com>
2014-11-06 11:07:35 +0100

    Changed input type to text (readonly)

--- ClientSide/view/DivScore.php
+++ ClientSide/view/DivScore.php
@@ -5,7 +5,7 @@
 			<tr>
 <!-- 			Technologie -->
 				<td class="scoreName" data-ng-bind="scoreTechnologyTxt"></td>
-				<td class="scoreNumber"><input type="number" id="table_technology_number" readonly="readonly" data-ng-value="scoreTechnology"></td>
+				<td class="scoreNumber"><input type="text" id="table_technology_number" readonly="readonly" data-ng-value="scoreTechnology"></td>
 				
 <!-- 				some text to display -->
 				<td rowspan="7" id="textEndOfGame" data-ng-bind="scoreTxt"></td>
@@ -13,28 +13,28 @@
 			<tr>
 <!-- 			Wealth -->
 				<td class="scoreName"  data-ng-bind="scoreWealthTxt"></td>
-				<td class="scoreNumber"><input type="number" id="table_wealth_number" readonly="readonly" data-ng-value="scoreWealth"></td>
+				<td class="scoreNumber"><input type="text" id="table_wealth_number" readonly="readonly" data-ng-value="scoreWealth"></td>
 			</tr>
 			<tr>
 <!-- 			Buildings -->
 				<td class="scoreName" data-ng-bind="scoreBuildingsTxt"></td>
-				<td class="scoreNumber"><input type="number" id="table_buildings_number" readonly="readonly" data-ng-value="scoreBuildings"></td>
+				<td class="scoreNumber"><input type="text" id="table_buildings_number" readonly="readonly" data-ng-value="scoreBuildings"></td>
 			</tr>
 			<tr>
 <!-- 			Population -->
 				<td class="scoreName" data-ng-bind="scorePopulationTxt"></td>
-				<td class="scoreNumber"><input type="number" id="table_population_number" readonly="readonly" data-ng-value="scorePopulation"></td>
+				<td class="scoreNumber"><input type="text" id="table_population_number" readonly="readonly" data-ng-value="scorePopulation"></td>
 			</tr>
 			<tr>
 <!-- 			Happiness -->
 				<td class="scoreName" data-ng-bind="scoreHappinessTxt"></td>
-				<td class="scoreNumber"><input type="number" id="table_happyness_number" readonly="readonly" data-ng-value="scoreHappyness"></td>
+				<td class="scoreNumber"><input type="text" id="table_happyness_number" readonly="readonly" data-ng-value="scoreHappyness"></td>
 			</tr>
 			<tr class="scoreName"></tr>
 			<tr>
 <!-- 			Total -->
 				<td class="scoreName" data-ng-bind="scoreTotalTxt"></td>
-				<td class="scoreNumber"><input type="number" id="table_total_number" readonly="readonly" data-ng-value="scoreTotal"></td>
+				<td class="scoreNumber"><input type="text" id="table_total_number" readonly="readonly" data-ng-value="scoreTotal"></td>
 			</tr>
 		</tbody>
 
